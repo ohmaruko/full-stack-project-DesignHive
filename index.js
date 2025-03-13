6303f5e451c1c5df963e0b933d65e1eea62f6a6e
madoka <mnogaki@my.bcit.ca>
2025-03-13 16:47:50 -0700

    add log in page content and basic functionality

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 import express from "express"
 import pg from "pg"
+import session from "express-session";
 
 const app = express();
 const port = 3000;
 
 
 // middleware
-app.use(express.urlencoded({extended: true}))//to make the body accessible
+app.use(express.urlencoded({extended: true}))//to make req.body accessible
 app.use(express.static('public'));//to make everything in the public folder accessible
+app.use(session({
+    secret: 'design_hive',
+    resave: false,
+    saveUninitialized: true,
+}))
 
 // database
-const bd = new pg.Client({
+const db = new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "DesignHive",
     password: "1234",
     port: 5432,
 })
+db.connect();
 
 // Log in
 app.get('/', (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs");    
+    console.log(req.session);
 })
-app.post('/feed', (req, res) => {
-    res.redirect("/feed");
+app.post('/', async (req, res) => {
+    const { username, password } = req.body;//user's input
+    console.log(`username input: ${username}, password input: ${password}`);
+    const userQueryResult = await db.query("SELECT * FROM users WHERE user_name=$1", [username]);//query data from username input
+    console.log(userQueryResult.rows);
+
+    if(password == userQueryResult.rows[0].password){
+        console.log("success")
+        res.redirect("/feed");
+    } else {
+        res.render("index.ejs", {errorMessage: "Invalid username or password. Please try again."})
+    }
 })
 
 // Feed
